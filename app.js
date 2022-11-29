cee1d794824ebc9e7cf08656a5b8149e3a09dff4
yanaleytvel <94953373+yanaleytvel@users.noreply.github.com>
2022-11-29 20:55:25 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+document.body.onload = function() {
+    console.log('hello onload')
+    let request = new XMLHttpRequest;
+    request.open('GET', './header.html', true)
+    request.send()
+    request.onreadystatechange = function () {
+        if (request.readyState === 4)
+        if (request.status === 200) {
+            console.log(request.responseText)
+            document.body.innerHTML = request.responseText + document.body.innerHTML
+        }
+    }
+    let request2 = new XMLHttpRequest;
+    request2.open('GET', 'footer.html', true)
+    request2.send()
+    request2.onreadystatechange = function () {
+        if (request2.readyState === 4)
+        if (request2.status === 200) {
+            console.log(request2.responseText)
+            document.body.innerHTML = document.body.innerHTML + request2.responseText  
+        }
+    }
+}
